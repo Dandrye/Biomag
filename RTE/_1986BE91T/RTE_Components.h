e30fadec4d63e96fba640b440870555a0d9f385e
Dandrye <54003241+Dandrye@users.noreply.github.com>
2024-03-04 13:46:33 +0300

    Add files via upload

---
+++ RTE/_1986BE91T/RTE_Components.h
@@ -0,0 +1,19 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Biomag' 
+ * Target:  '1986BE91T' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+/*  Keil::Device:Startup:2.0.3 */
+/* Target microcontroller definition */
+#define USE_MDR1986VE9x
+#define USE_MDR1986VE91
+
+
+#endif /* RTE_COMPONENTS_H */
